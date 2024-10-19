defdc0a5aaa6628740f7f45a01e4709fc394c042
ASHISH SONAVANE <90111868+Ashishsonavane@users.noreply.github.com>
2024-10-20 04:32:16 +0530

    Update global-prefix.spec.ts

--- integration/nest-application/global-prefix/e2e/global-prefix.spec.ts
+++ integration/nest-application/global-prefix/e2e/global-prefix.spec.ts
@@ -146,3 +146,15 @@ describe('Global prefix', () => {
     await app.close();
   });
 });
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+
+</body>
+</html>
